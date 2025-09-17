2d82c522ac9fe6794ef5ac93fbb763275da84ce9
rgbcmy <1796442761@qq.com>
2025-09-17 15:35:07 +0800

    update IMap entity

--- src/dto/map.ts
+++ src/dto/map.ts
@@ -2,18 +2,18 @@ import type { ISerializedView } from "./view";
 import type { ITileGrid } from "./source";
 import type {IBaseLayer, ISerializedLayer} from "./layer";
 export interface IMap{
-    name:string,
+    name:string;
     //控件列表
-    controls:any[],
+    controls:any[];
     //物理像素和dpi的比率，自动根据设备获取和设置，故不序列化它
     //pixelRatio:number,
     //交互列表
-    interactions:any[],
-    keyboardEventTarget?: string,
-    layers:IBaseLayer[],
-    maxTilesLoading?: number,
-    moveTolerance?: number,
-    overlays?:any[],
-    target?: string,
-    view: ISerializedView
+    interactions:any[];
+    keyboardEventTarget?: string;
+    layers:IBaseLayer[];
+    maxTilesLoading?: number;
+    moveTolerance?: number;
+    overlays?:any[];
+    target?: string;
+    view: ISerializedView;
 }
